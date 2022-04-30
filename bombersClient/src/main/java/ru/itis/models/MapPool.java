5b6c89c25c8b6df4c5954d43691c07666a1b9b63
Arthur <rthoor@mail.ru>
2022-04-30 13:23:21 +0300

    FIXED BUGS

--- bombersClient/src/main/java/ru/itis/models/MapPool.java
+++ bombersClient/src/main/java/ru/itis/models/MapPool.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public enum MapPool {
-    CASTLE("src/main/resources/img/blocks/grass.png", "src/main/resources/img/blocks/grey.png",
+    CASTLE("bombersClient/src/main/resources/img/blocks/grass.png", "bombersClient/src/main/resources/img/blocks/grey.png",
             new Cell(1, 10), new Cell(10, 1),
             Arrays.asList(new Cell(2,2), new Cell(2,4), new Cell(2,5), new Cell(2,8), new Cell(2,9),
                     new Cell(1,8), new Cell(4,2), new Cell(4,7), new Cell(4,8), new Cell(4,6),
@@ -24,7 +24,7 @@ public enum MapPool {
                     new Cell(6,11), new Cell(7,11), new Cell(8,11), new Cell(9,11),
                     new Cell(10,11))),
 
-    SAND("src/main/resources/img/blocks/sand.png", "src/main/resources/img/blocks/brown.png",
+    SAND("bombersClient/src/main/resources/img/blocks/sand.png", "bombersClient/src/main/resources/img/blocks/brown.png",
             new Cell(9, 10), new Cell(2, 1),
             Arrays.asList(new Cell(3,1), new Cell(8,1), new Cell(2,2), new Cell(4,3), new Cell(5,3),
                     new Cell(7,3), new Cell(9,3), new Cell(10,3), new Cell(4,4), new Cell(7,4),
@@ -45,7 +45,7 @@ public enum MapPool {
                     new Cell(6,11), new Cell(7,11), new Cell(8,11), new Cell(9,11),
                     new Cell(10,11))),
 
-    NEON("src/main/resources/img/blocks/blue.png", "src/main/resources/img/blocks/pink.png",
+    NEON("bombersClient/src/main/resources/img/blocks/blue.png", "bombersClient/src/main/resources/img/blocks/pink.png",
             new Cell(9, 1), new Cell(2, 10),
             Arrays.asList(new Cell(4,1), new Cell(10,1), new Cell(2,2), new Cell(5,2), new Cell(7,2),
                     new Cell(9,2), new Cell(3,3), new Cell(8,3), new Cell(4,4), new Cell(6,4),
@@ -66,7 +66,7 @@ public enum MapPool {
                     new Cell(6,11), new Cell(7,11), new Cell(8,11), new Cell(9,11),
                     new Cell(10,11))),
 
-    JUNGLE("src/main/resources/img/blocks/grass.png", "src/main/resources/img/blocks/darkGreen.png",
+    JUNGLE("bombersClient/src/main/resources/img/blocks/grass.png", "bombersClient/src/main/resources/img/blocks/darkGreen.png",
             new Cell(10, 6), new Cell(1, 4),
             Arrays.asList(new Cell(2,1), new Cell(3,1), new Cell(7,1), new Cell(9,1), new Cell(5,2),
                     new Cell(6,2), new Cell(7,2), new Cell(1,3), new Cell(2,3), new Cell(4,3),
